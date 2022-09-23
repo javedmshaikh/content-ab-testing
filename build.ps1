d775676374f3464bb886c54101dcc3d201ca8bc0
javed-vm-demos\azureadmin <javed.shaikh@gmail.com>
2022-09-23 10:08:28 -0700

    Checking in fixes related to build and package

--- build.ps1
+++ build.ps1
@@ -6,4 +6,5 @@ yarn --cwd src/EPiServer.Marketing.KPI.Commerce/clientResources build
 yarn --cwd src/EPiServer.Marketing.Testing.Web/clientResources/config install
 yarn --cwd src/EPiServer.Marketing.Testing.Web/clientResources/config build
 dotnet build -c $mode
-./build/pack.ps1 -configuration $mode && ./build/repack.ps1
+./build/pack.ps1 -configuration $mode 
+./build/repack.ps1
