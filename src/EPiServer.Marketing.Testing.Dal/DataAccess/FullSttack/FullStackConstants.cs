5295e5acfee579fa80b405bdd4cb166c62c6f65f
javed-vm-demos\azureadmin <javed.shaikh@gmail.com>
2022-10-11 11:21:15 -0700

    Checking in missing file

---
+++ src/EPiServer.Marketing.Testing.Dal/DataAccess/FullSttack/FullStackConstants.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EPiServer.Marketing.Testing.Dal.DataAccess.FullSttack
+{
+    public static class FullStackConstants
+    {
+        public static string ProjectId = "21972070188";
+
+        public static string EnviromentKey  = "production";
+
+        public static string EventName = "page_view";
+
+        public static string EventDescription = "Event to calculate page view metrics";
+
+        public static int APIVersion = 1;
+
+        public static string ReplaceABTestExperiment = "AB_Test_Experiment";
+
+        public static string ReplaceExperimentFlag = "_Experiment_Flag";
+
+        public static string FullStackUserGUID = "FullStackUserGUID";
+    }
+}
